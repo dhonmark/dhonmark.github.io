e89f172b1df8ff32da2f3914c2b75a45d6d85175
dhonmark <managbanagdosal@gmail.com>
2023-11-04 11:24:37 +0800

    added files

---
+++ app.js
@@ -0,0 +1,18 @@
+/* My Works tab content */
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+/* Compact Galley Function */
+baguetteBox.run('.compact-gallery', { animation: 'slideIn' });
+
